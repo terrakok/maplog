4b5ac3a218650881ae2b5f0fb67e9fecf1820de7
terrakok <terrakok@gmail.com>
2014-04-08 12:50:02 +0400

    reformat code AND newTrack photo upload

--- public/js/joinModule.js
+++ public/js/joinModule.js
@@ -4,96 +4,96 @@ var maplogJoin = angular.module('maplogJoin', []);
 
 maplogJoin.controller('joinCtrl', function ($scope) {
 
-    $scope.registration = function () {
-        var nickName = $scope.nick;
-        var email = $scope.email;
-        var pass = $scope.password;
+  $scope.registration = function () {
+    var nickName = $scope.nick;
+    var email = $scope.email;
+    var pass = $scope.password;
 
-        if (!validateEmail(email)) {
-            alert('Check your email address!');
-        } else if (!validateNick(nickName)) {
-            alert('Check your Nickname!');
-        } else if (!validatePassword(pass)) {
-            alert('Check your Password!');
+    if (!validateEmail(email)) {
+      alert('Check your email address!');
+    } else if (!validateNick(nickName)) {
+      alert('Check your Nickname!');
+    } else if (!validatePassword(pass)) {
+      alert('Check your Password!');
+    } else {
+      var xhr = new XMLHttpRequest();
+      var params = 'usernick=' + encodeURIComponent(nickName) + '&useremail=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(pass);
+      xhr.onloadend = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+          var jsonData = JSON.parse(xhr.responseText);
+          if (!jsonData.error) {
+            alert('Registration OK! nick = ' + jsonData.usernick);
+          } else {
+            alert("Error: " + jsonData.error);
+          }
         } else {
-            var xhr = new XMLHttpRequest();
-            var params = 'usernick=' + encodeURIComponent(nickName) + '&useremail=' + encodeURIComponent(email) + '&password=' + encodeURIComponent(pass);
-            xhr.onloadend = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    var jsonData = JSON.parse(xhr.responseText);
-                    if (!jsonData.error) {
-                        alert('Registration OK! nick = ' + jsonData.usernick);
-                    } else {
-                        alert("Error: " + jsonData.error);
-                    }
-                } else {
-                    alert("Server not found!");
-                }
-            };
-            xhr.open('POST', SERVER_ADDRESS + 'api/registration', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.send(params);
+          alert("Server not found!");
         }
+      };
+      xhr.open('POST', SERVER_ADDRESS + 'api/registration', true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.send(params);
     }
+  }
 });
 
 function validateNick(nickName) {
-    var re = /\w{3,24}/;
-    return re.test(nickName);
+  var re = /\w{3,24}/;
+  return re.test(nickName);
 }
 
 function validateEmail(email) {
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
 
 function validatePassword(pass) {
-    var re = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/;
-    return re.test(pass);
+  var re = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/;
+  return re.test(pass);
 }
 
 function redirect(addr) {
-    document.location.href = '' + addr + '.html';
+  document.location.href = '' + addr + '.html';
 }
 
 function getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+  var matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 function deleteCookie(name) {
-    setCookie(name, "", {
-        expires: -1
-    })
+  setCookie(name, "", {
+    expires: -1
+  })
 }
 
 function setCookie(name, value, options) {
-    options = options || {};
+  options = options || {};
 
-    var expires = options.expires;
+  var expires = options.expires;
 
-    if (typeof expires == "number" && expires) {
-        var d = new Date();
-        d.setTime(d.getTime() + expires * 1000);
-        expires = options.expires = d;
+  if (typeof expires == "number" && expires) {
+    var d = new Date();
+    d.setTime(d.getTime() + expires * 1000);
+    expires = options.expires = d;
+  }
+  if (expires && expires.toUTCString) {
+    options.expires = expires.toUTCString();
+  }
+
+  value = encodeURIComponent(value);
+
+  var updatedCookie = name + "=" + value;
+
+  for (var propName in options) {
+    updatedCookie += "; " + propName;
+    var propValue = options[propName];
+    if (propValue !== true) {
+      updatedCookie += "=" + propValue;
     }
-    if (expires && expires.toUTCString) {
-        options.expires = expires.toUTCString();
-    }
+  }
 
-    value = encodeURIComponent(value);
-
-    var updatedCookie = name + "=" + value;
-
-    for (var propName in options) {
-        updatedCookie += "; " + propName;
-        var propValue = options[propName];
-        if (propValue !== true) {
-            updatedCookie += "=" + propValue;
-        }
-    }
-
-    document.cookie = updatedCookie;
+  document.cookie = updatedCookie;
 }
--- public/js/newTrackModule.js
+++ public/js/newTrackModule.js
@@ -3,98 +3,102 @@
 var maplogNewTrack = angular.module('maplogNewTrack', ['angularFileUpload', 'ui.sortable']);
 
 maplogNewTrack.controller('newTrackCtrl', function ($scope, $upload) {
-    var map;
-    $scope.allFilesArr = [];
-    $scope.uploadFiles = [];
+  var map;
+  $scope.allFilesArr = [];
+  $scope.uploadFiles = [];
+  $scope.sortableOptions = {
+    axis: 'x'
+  };
 
-    initMap();
+  initMap();
 
-    $scope.sortableOptions = {
-        axis: 'x'
-    };
+  $scope.onFileSelect = function ($files) {
+    var newFiles = $files;
+    var filesCounter = $scope.allFilesArr.length;
 
-    $scope.onFileSelect = function ($files) {
-        var newFiles = $files;
-        var filesCounter = $scope.allFilesArr.length;
+    //добавляем все изображения в общий массив
+    for (var i = 0; i < newFiles.length; i++) {
+      var file = newFiles[i];
 
-        //добавляем все изображения в общий массив
-        for (var i = 0; i < newFiles.length; i++) {
-            var file = newFiles[i];
+      if (window.FileReader && file.type.indexOf('image') > -1) {
+        var file_key = filesCounter + i;
 
-            if (window.FileReader && file.type.indexOf('image') > -1) {
-                var file_key = filesCounter + i;
-
-                var fileInfo = {
-                    latitude: null,
-                    longitude: null,
-                    description: 'File name: ' + file.name,
-                    file_id: null,
-                    file_key: file_key
-                }
-
-                $scope.allFilesArr.push(fileInfo);
-
-                (function (key) {
-                    $scope.uploadFiles[key] = $upload.upload({
-                        url: SERVER_ADDRESS + 'api/upload_new_photo',
-                        method: 'POST',
-                        file: file,
-                        fileFormDataName: 'newPhoto'
-                    }).progress(function (evt) {
-                        // get upload percentage
-                        console.log(key + ': percent = ' + parseInt(100.0 * evt.loaded / evt.total));
-                    }).success(function (data, status, headers, config) {
-                        // file is uploaded successfully
-                        console.log(data);
-                        if (status === 200) {
-                            var jsonData = JSON.parse(data);
-                            updateThumbnail(key, jsonData.fileID);
-                        } else {
-                            updateThumbnail(key, '');
-                        }
-                    }).error(function (data, status, headers, config) {
-                        // file failed to upload
-                        console.log(data);
-                        updateThumbnail(key, '');
-                    });
-                })(file_key);
-            }
+        var fileInfo = {
+          latitude: null,
+          longitude: null,
+          description: 'File name: ' + file.name,
+          file_id: null,
+          file_key: file_key
         }
-        $scope.$apply();
-    }
 
-    $scope.abortUploadFile = function (file_key) {
-        $scope.uploadFiles[file_key].abort();
-    }
+        $scope.allFilesArr.push(fileInfo);
 
-    $scope.getThumbnail = function (fileID) {
-        if (fileID) {
-            if (fileID.length > 0) {
-                return SERVER_ADDRESS + 'api/photo/' + fileID + '.thumb'
+        (function (key) {
+          $scope.uploadFiles[key] = $upload.upload({
+            url: SERVER_ADDRESS + 'api/upload_new_photo',
+            method: 'POST',
+            file: file,
+            fileFormDataName: 'newPhoto'
+          }).progress(function (evt) {
+            // get upload percentage
+            console.log(key + ': percent = ' + parseInt(100.0 * evt.loaded / evt.total));
+          }).success(function (data, status, headers, config) {
+            if (status === 200) {
+              updateThumbnail(key, data.fileID);
             } else {
-                return SERVER_ADDRESS + 'img/errorThumb.jpg'
+              updateThumbnail(key, '');
             }
-        } else {
-            return SERVER_ADDRESS + 'img/loadingThumb.jpg'
-        }
+          }).error(function (data, status, headers, config) {
+            updateThumbnail(key, '');
+          });
+        })(file_key);
+      }
     }
+    $scope.$apply();
+  }
 
-    function updateThumbnail(file_key, fileID) {
-        $scope.allFilesArr.forEach(function (element, index, array) {
-            if (element.file_key === file_key) {
-                element.file_id = fileID;
-            }
-        });
-        $scope.$apply();
+  $scope.abortUploadFile = function (file_key) {
+    $scope.uploadFiles[file_key].abort();
+  }
+
+  $scope.getThumbnail = function (fileID) {
+    if (fileID) {
+      if (fileID.length > 0) {
+        return SERVER_ADDRESS + 'api/photo/' + fileID + '.thumb'
+      } else {
+        return SERVER_ADDRESS + 'img/errorThumb.jpg'
+      }
+    } else {
+      return SERVER_ADDRESS + 'img/loadingThumb.jpg'
     }
+  }
 
-    function initMap() {
-        // create the tile layer with correct attribution
-        var osmUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
-        var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
-        var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 15, attribution: osmAttrib});
+  function updateThumbnail(file_key, fileID) {
+    $scope.safeApply($scope.allFilesArr.forEach(function (element, index, array) {
+      if (element.file_key === file_key) {
+        element.file_id = fileID;
+      }
+    }));
+  }
 
-        map = L.map('map').setView([51.505, -0.09], 13);
-        map.addLayer(osm);
+  function initMap() {
+    // create the tile layer with correct attribution
+    var osmUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+    var osmAttrib = 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 15, attribution: osmAttrib});
+
+    map = L.map('map').setView([51.505, -0.09], 13);
+    map.addLayer(osm);
+  }
+
+  $scope.safeApply = function (fn) {
+    var phase = this.$root.$$phase;
+    if (phase == '$apply' || phase == '$digest') {
+      if (fn && (typeof(fn) === 'function')) {
+        fn();
+      }
+    } else {
+      this.$apply(fn);
     }
+  };
 });
